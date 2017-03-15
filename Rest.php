39f03c32e9247fb3ec6be61121a4a7c00d6176ef
lilianabarrosc <lilianabarrosc@gmail.com>
2017-03-15 15:46:20 -0300

    Nueva clase
    Se termino la clase rest y se creo una nueva clase con extension
    htaccess para hacer las URL's más amigables.
    Con esta regla de reescritura estamos especificando:
    - El directorio base es /login_restful/
    - Se han añadido tres condiciones para restringir la reescritura sólo a
    rutas que no existan previamente.
    La primera condición previene los directorios que ya existan con la
    bandera !-d. La segunda condición hace que se ignoren ficheros que ya
    existan con la bandera !-f. Y la tercera condición hace que se ignoren
    los enlaces simbólicos que ya existan con !-l.
    -Luego con la regla de reescritura transformaremos una URL amigable a
    una URL con la que el servidor pueda trabajar.

--- Rest.php
+++ Rest.php
@@ -1,56 +1,56 @@
 <?php
-//clase Rest, se encarga de:
-//-Devolver cabeceras y resultados
-//-Filtrar/procesar los Datos
+#clase Rest, se encarga de:
+#-Devolver cabeceras y resultados
+#-Filtrar/procesar los Datos
 
 class Rest{
-	//tipo de dato que se aceptaran
+	#tipo de dato que se aceptaran
 	public $tipoDato = "application/json";
-	//Array con los datos solicitados
-	public $datosPeticion = arra();
-	//Estado por defecto de respuesta Ok HTTP
+	#Array con los datos solicitados
+	public $datosPeticion = array();
+	#Estado por defecto de respuesta Ok HTTP
 	private $_codEstado = 200;
 
-	//metodo principal para tratar los datos de entrada
+	#metodo principal para tratar los datos de entrada
 	public function constructor(){
 		$this->tratarEntrada();
 	}
 
-	//metodo encargado de mostrar la respuesta al usuario
-	//recibe como entrada los parametros $data, que contiene la respuesta JSON a enviar
-	// y  $estado que especifica el código de estado HTTP
+	#metodo encargado de mostrar la respuesta al usuario
+	#recibe como entrada los parametros $data, que contiene la respuesta JSON a enviar
+	# y  $estado que especifica el código de estado HTTP
 	public function respuesta($data, $estado){
-		$this->_codEstado = ($estado)? $estado:200; //si estado esta vacio toma el valor por defecto = 200
-		//se obtiene la cabecera HTTP
+		$this->_codEstado = ($estado)? $estado:200; #si estado esta vacio toma el valor por defecto = 200
+		#se obtiene la cabecera HTTP
 		$this->setCabecera();
 		echo $data;
 		exit;
 	}
 
-	//metodo encargado de crear dos cabeceras de respuesta dependiendo del estado
+	#metodo encargado de crear dos cabeceras de respuesta dependiendo del estado
 	public function setCabecera(){
-		//ejemplo = HTTP/1.1 201 Created
+		#ejemplo = HTTP/1.1 201 Created
 		header("HTTP/1.1" . $this->_codEstado . $this->getCodEstado());
 		header("Content-Type:" . $this->tipoDato . ';charset=utf-8');
-		//Content-Type: application/x-www-form-urlencoded ??
-		//Accept: application/x-www-form-urlencoded;application/json;charset=UTF-8;q=0.5
+		#Content-Type: application/x-www-form-urlencoded ??
+		#Accept: application/x-www-form-urlencoded;application/json;charset=UTF-8;q=0.5
 	}
 
-	//Lista con los codigos HTTP con su respectiva descripcion
+	#Lista con los codigos HTTP con su respectiva descripcion
 	private function getCodEstado(){
 		$estado = array(
-			//Success
+			#Success
 			200 => 'OK', 
 			201 => 'Created',
 			202 => 'Accepted',
 			202 => 'Non-Authoritative Information',
 			204 => 'No Content',
-			//Redirection
+			#Redirection
 			301 => 'Moved Permanently',
 			302 => 'Found',
 			303 => 'See Other',
 			304 => 'Not Modified',
-			//Client Error
+			#Client Error
 			400 => 'Bad Request',
 			401 => 'Unauthorized',
 			403 => 'Forbidden',
@@ -60,7 +60,7 @@ class Rest{
 			408 => 'Request Timeout',
 			409 => 'Conflict',
 			429 => 'Too Many Requests',
-			//Server Error
+			# Server Error
 			500 => 'Internal Server Error',
 			501 => 'Not Implemented',
 			502 => 'Bad Gateway',
@@ -70,6 +70,59 @@ class Rest{
 
 		$resp = ($estado[$this->_codEstado])? $estado[$this->_codEstado]:$estado[500]; //por defecto se envia el estado 500
 		return $resp;
+	}
+
+	# Metodo que se encarga de procesar el array de entrada y devuelve los datos solicitados segun get o post  
+	private function tratarEntrada(){
+		$metodo = $_SERVER['REQUEST_METHOD'];
+
+		# se trataran tres casos post, get, delete y put (update se tratara despues)
+		switch ($metodo) {
+			case "GET":
+				$this->datosPeticion = $this->limpiarEntrada($_GET); 
+				break;	
+			case "POST":
+				$this->datosPeticion = $this->limpiarEntrada($_POST); 
+				break;
+			case "DELETE": //"falling though". Se ejecutará el case siguiente  
+			case "PUT":
+				//php no tiene un método propiamente dicho para leer una petición PUT o DELETE por lo que se usa un "truco":  
+		        //leer el stream de entrada file_get_contents("php://input") que transfiere un fichero a una cadena.  
+		        //Con ello obtenemos una cadena de pares clave valor de variables (variable1=dato1&variable2=data2...)
+		        //que evidentemente tendremos que transformarla a un array asociativo.  
+		        //Con parse_str meteremos la cadena en un array donde cada par de elementos es un componente del array.  
+				parse_str(file_get_contents("php://input"), $this->datosPeticion);  
+         		$this->datosPeticion = $this->limpiarEntrada($this->datosPeticion); 
+				break;
+			default:
+				$this->response('',404);
+				break;
+		}
+	}
+
+	//metodo encargado de parsear los datos que se pasen por prámetro
+	public function limpiarEntrada($data){
+		$entrada = array(); //datos de la entrada
+
+		if (is_array($data)) {
+			foreach ($data as $key => $value) {
+				$entrada[$key] = $this->limpiarEntrada($value);
+			}
+		} else{
+			if (get_magic_quotes_gpc()) {
+				//Quitamos las barras de un string con comillas escapadas  
+		        //Aunque actualmente se desaconseja su uso, muchos servidores tienen activada la extensión magic_quotes_gpc.   
+		        //Cuando esta extensión está activada, PHP añade automáticamente caracteres de escape (\) delante de las comillas que se escriban en un campo de formulario.   
+		        $data = trim(stripslashes($data));
+			}
+
+			#eliminamos etiquetas html y php
+			$data = strip_tags($data);
+			#se convierten los caracteres a entidades HTML
+			$data = htmlentities($data);
+			$entrada = trim($data);
+		}
+		return $entrada;
 	}
 }
 
