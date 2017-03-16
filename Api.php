f1523b4e0bd163c1cc966f5b811217c1122c992c
lilianabarrosc <lilianabarrosc@gmail.com>
2017-03-15 23:07:25 -0300

    Variables
    Definición de variables a utilizar en la clase

--- Api.php
+++ Api.php
@@ -1,1 +1,18 @@
-Api
+<?php
+# Se utilizarán los métodos de la clase Rest creada anteriormente
+require_once("Rest.php");
+
+class Api extends Rest{
+	# valores fijos del servicio
+	const servidor = "localhost";
+	const usuario_db = "postgress";
+	const pwd_db = "";
+	const nombre_db = "autorizacion";
+	#variables globales
+	private $_conn = NULL;
+	private $_metodo;
+	private $argumentos;
+
+}
+
+?>
