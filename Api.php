e9134b459ecc4f56ed99b07584a761b6e54323f7
lilianabarrosc <lilianabarrosc@gmail.com>
2017-03-17 22:08:21 -0300

    Funciones para acceder y cerrar la base de datos

--- Api.php
+++ Api.php
@@ -6,6 +6,7 @@ class Api extends Rest{
 	# valores fijos del servicio
 	const servidor = "localhost";
 	const usuario_db = "postgress";
+	const port = "5432";
 	const pwd_db = "";
 	const nombre_db = "autorizacion";
 	#variables globales
@@ -13,6 +14,27 @@ class Api extends Rest{
 	private $_metodo;
 	private $argumentos;
 
+	//metodo principal que hace llamado a la funcionalidad padre y conecta a la bd
+	public function constructor(){
+		parent::constructor();
+		$this->conectarDB();
+	}
+
+	private function conectarDB(){
+		$dsn = "host=".servidor." port=".port." user=".usuario_db." pass=".pwd_db." dbname=".nombre_db;
+    	try{
+    		$this->_conn = pg_connect(
+    	}
+    $connect = pg_connect("host=$host, port=$port, user=$user, 
+pass=$pass, dbname=$dbname");
+
+    if(!$connect)
+        echo "<p><i>No me conecte</i></p>";
+    else
+        echo "<p><i>Me conecte</i></p>";
+
+    pg_close($connect);
+	}
 }
 
 ?>
