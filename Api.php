1ff5b36b417bcd28766ac2cd67c54b1e3d57cc01
Unknown <lilianabarrosc@gmail.com>
2017-03-19 23:41:04 -0300

    Funcion que procesa la peticion URL

--- Api.php
+++ Api.php
@@ -48,6 +48,33 @@ class Api extends Rest{
 			);
 		return $errores[$id];
 	}
+
+	//metodo encargado de procesar la URL de la peticion y llamar al metodo que lo resolvera
+	public function procesarLlamada(){
+		if(isset($_REQUEST['url'])){
+			//si por ejemplo pasamos explode('/','////controller///method////args///') el resultado es un array con elem vacios;
+	        //Array ( [0] => [1] => [2] => [3] => [4] => controller [5] => [6] => [7] => method [8] => [9] => [10] => [11] => args [12] => [13] => [14] => )
+	        $url = explode('/', trim($_REQUEST['url']));
+	        //con array_filter() filtramos elementos de un array pasando función callback, que es opcional.
+	        //si no le pasamos función callback, los elementos false o vacios del array serán borrados 
+ 	        //por lo tanto entre la anterior función (explode) y esta eliminamos los '/' sobrantes de la URL
+	        $url = array_filter($url); 
+	        $this->_metodo = strtolower(array_shift($url));  
+       		$this->_argumentos = $url;  
+       		$func = $this->_metodo;
+ 
+       		if((int) method_exists($this, $func) > 0){
+       			//Si la URL tiene argumentos ej borrarUsuario/1 (1->argumento)
+       			if (count($this->_argumentos) > 0) {
+       				call_user_func_array(array($this, $this->_metodo), $this->_argumentos);
+       			} else {//si no, lo llamamos sin argumentos, al metodo del controlador ej http://localhost/login_rest/usuarios 
+           			call_user_func(array($this, $this->_metodo));  
+         		}  
+       		} else 
+       			$this->mostrarRespuesta($this->convertirJson($this->devolverError(0)), 404);
+		}
+		$this->mostrarRespuesta($this->convertirJson($this->devolverError(0)), 404);
+	}
 }
 
 ?>
