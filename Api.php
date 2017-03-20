e62b86109fb11f13ce52dbce5ef39ec0cdfa6d4b
Unknown <lilianabarrosc@gmail.com>
2017-03-20 10:41:54 -0300

    Metodos de convertir un objeto a un json y devolver la primera peticion del cliente "lista de usuarios"

--- Api.php
+++ Api.php
@@ -51,15 +51,15 @@ class Api extends Rest{
 
 	//metodo encargado de procesar la URL de la peticion y llamar al metodo que lo resolvera
 	public function procesarLlamada(){
-		if(isset($_REQUEST['url'])){
+		if(isset($_REQUEST['url'])){ //si trae un respuesta !null
 			//si por ejemplo pasamos explode('/','////controller///method////args///') el resultado es un array con elem vacios;
 	        //Array ( [0] => [1] => [2] => [3] => [4] => controller [5] => [6] => [7] => method [8] => [9] => [10] => [11] => args [12] => [13] => [14] => )
 	        $url = explode('/', trim($_REQUEST['url']));
 	        //con array_filter() filtramos elementos de un array pasando función callback, que es opcional.
 	        //si no le pasamos función callback, los elementos false o vacios del array serán borrados 
- 	        //por lo tanto entre la anterior función (explode) y esta eliminamos los '/' sobrantes de la URL
+ 	        //por lo tanto entre la anterior función (explode) y esta eliminamos los '/' sobrantes de la URL -> devuelve indice/valor en un array
 	        $url = array_filter($url); 
-	        $this->_metodo = strtolower(array_shift($url));  
+	        $this->_metodo = strtolower(array_shift($url)); //transforma a minusculas y se quita el primer elemento del array
        		$this->_argumentos = $url;  
        		$func = $this->_metodo;
  
@@ -70,10 +70,36 @@ class Api extends Rest{
        			} else {//si no, lo llamamos sin argumentos, al metodo del controlador ej http://localhost/login_rest/usuarios 
            			call_user_func(array($this, $this->_metodo));  
          		}  
-       		} else 
+       		} else // se envia el error en formato json
        			$this->mostrarRespuesta($this->convertirJson($this->devolverError(0)), 404);
 		}
+		//se envia el error en formato json
 		$this->mostrarRespuesta($this->convertirJson($this->devolverError(0)), 404);
+	}
+
+	//metodo que devuelve un json
+	private function convertirJson($data){
+		return json_encode($data);
+	}
+
+	//metodo encargado de procesar la peticion y devuelve un json con los datos de los usuarios
+	public function usuarios(){
+		if ($_SERVER['REQUEST_METHOD'] != "GET") {
+			# no se envian los usuarios, se envia un error
+			$this->mostrarRespuesta($this->convertirJson($this->devolverError(1)), 405);
+		}//es una petición GET
+		//se realiza la consulta a la bd
+		$query = $this->_conn->query("SELECT id, nombre, email FROM usuario");
+		//cantidad de usuarios
+		$filas = $query->fetchAll(PDO::FETCH_ASSOC);  
+     	$num = count($filas);
+     	//si devolvio un resultado, se envia al cliente
+     	if ($num > 0) {  
+	       $respuesta['estado'] = 'correcto';  
+	       $respuesta['usuarios'] = $filas;  
+	       $this->mostrarRespuesta($this->convertirJson($respuesta), 200);  
+	     } //se envia un error  
+	     $this->mostrarRespuesta($this->devolverError(2), 204);
 	}
 }
 
